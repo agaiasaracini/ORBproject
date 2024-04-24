2f662088908b38674a1795aa7303aae90dc66a22
Alessandra <asaracini@ethz.ch>
2024-04-24 14:06:28 +0200

    additional helper wrapper function

--- Simulation/SummaryStatsHelper.R
+++ Simulation/SummaryStatsHelper.R
@@ -28,17 +28,63 @@ calculate_mse_mcse <- function(theta_hat, theta) {
 }
 
 # Function to calculate Coverage and MCSE
-calculate_coverage_mcse <- function(theta_low, theta_up, theta) {
-  N_sim <- length(theta_low)
-  coverage <- mean(theta_low <= theta & theta <= theta_up)
+calculate_coverage_mcse <- function(theta_hat_low, theta_hat_up, theta) {
+  N_sim <- length(theta_hat_low)
+  coverage <- mean(theta_hat_low <= theta & theta <= theta_hat_up)
   mcse <- sqrt(coverage * (1 - coverage) / N_sim)
   return(list(coverage = coverage, mcse = mcse))
 }
 
 # Function to calculate Power and MCSE
-calculate_power_mcse <- function(p_values) {
-  N_sim <- length(p_values)
-  power <- mean(p_values <= 0.05)
+calculate_power_mcse <- function(theta_hat_low, theta_hat_up) {
+  N_sim <- length(theta_hat_low)
+  power <- mean(theta_hat_low <= 0 & theta_hat_up >= 0)
   mcse <- sqrt(power * (1 - power) / N_sim)
   return(list(power = power, mcse = mcse))
 }
+
+
+
+############# Summary statistics function ######################################
+
+# Function to calculate summary statistics for a given method
+calculate_summary <- function(theta_hats, theta_hats_low, theta_hats_up, theta) {
+  
+  N_sim <- length(theta_hats)
+  
+  # Calculate bias and MCSE
+  bias_mcse <- calculate_bias_mcse(theta_hats, theta)
+  
+  # Calculate empirical SE and MCSE
+  emp_se_mcse <- calculate_empirical_se_mcse(theta_hats)
+  
+  # Calculate MSE and MCSE
+  mse_mcse <- calculate_mse_mcse(theta_hats, theta)
+  
+  # Calculate Coverage and MCSE
+  cov_mcse <- calculate_coverage_mcse(theta_hats_low, theta_hats_up, theta)
+  
+  # Calculate Power and MCSE
+  power_mcse <- calculate_power_mcse(theta_hats_low, theta_hats_up)
+  
+  
+  # Combine all the summary statistics into a list
+  summary <- list(
+    bias = bias_mcse$bias,
+    bias_mcse = bias_mcse$mcse,
+    emp_se = emp_se_mcse$empirical_se,
+    emp_se_mcse = emp_se_mcse$mcse,
+    mse = mse_mcse$mse,
+    mse_mcse = mse_mcse$mcse,
+    cov = cov_mcse$coverage,
+    cov_mcse = cov_mcse$mcse,
+    power = power_mcse$power,
+    power_mcse = power_mcse$mcse
+    
+  )
+  
+  return(summary)
+}
+
+
+
